a4dc9e57f64c2ff75ec1650acc60a8734eb16e7a
tommy <tommy@mytyresite.com>
2023-04-19 22:46:23 +1000

    added analytics to app

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,23 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
+import { Analytics } from '@vercel/analytics/react'
+
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 2000,
-            position: [ -3, 1.5, 4 ]
-        } }
-    >
-        <Experience />
-    </Canvas>
+    <>
+        <Canvas
+            camera={ {
+                fov: 45,
+                near: 0.1,
+                far: 2000,
+                position: [ -3, 1.5, 4 ]
+            } }
+        >
+            <Experience />
+        </Canvas>
+        <Analytics />
+    </>
 )
